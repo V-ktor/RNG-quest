26ed5e99bebe290164734e4d5c6bcdb35e74c0e7
Viktor Hahn <viktor.hahn@web.de>
2024-01-20 14:48:59 +0100

    Added character import function with drag and drop.

--- gui/menu.gd
+++ gui/menu.gd
@@ -7,10 +7,14 @@ var main:= preload("res://gui/main.tscn")
 var main_instance: Node
 var thread: Thread
 var tile_map_line:= 30
+var import_file:= ""
+var import_data: String
 
 @onready var progress_bar:= $Loading/ProgressBar
 @onready var progress_label:= $Loading/LabelProgress
 @onready var tile_map:= $TileMap
+@onready var import_name_override:= $ConfirmationDialog/LineEdit
+@onready var import_name_warning:= $ConfirmationDialog/LabelWarning
 
 
 func update_tile_map():
@@ -27,15 +31,18 @@ func update_tile_map():
 	tile_map_line -= 1
 
 
+func create_save_dir() -> DirAccess:
+	var dir:= DirAccess.open("user://")
+	var error:= dir.make_dir_recursive("user://saves")
+	if error!=OK:
+		print("Can't create save directory!")
+	return dir
+
 func load_characters():
 	var dir:= DirAccess.open("user://saves")
 	characters.clear()
 	if dir==null || DirAccess.get_open_error()!=OK:
-		var error: int
-		dir = DirAccess.open("user://")
-		error = dir.make_dir_recursive("user://saves")
-		if error!=OK:
-			print("Can't create save directory!")
+		create_save_dir()
 		return
 	
 	var file_name: String
@@ -106,6 +113,67 @@ func _export(ID: int):
 	file.close()
 	JavaScriptBridge.download_buffer(text.to_utf8_buffer(), characters[ID], "text/plain")
 
+func _files_dropped(file_names: Array[String]):
+	for file_name in file_names:
+		var file:= FileAccess.open(file_name, FileAccess.READ)
+		if not file:
+			print("Can't open file " + file_name)
+			continue
+		
+		var data: Dictionary = JSON.parse_string(file.get_line())
+		if !data.has("level") || !data.has("location") || !data.has("name") || !data.has("race"):
+			print("File " + file_name + " is not a valid save file")
+			continue
+		
+		# store the text because the dropped files may be temporary
+		import_data = file.get_as_text()
+		import_file = file_name
+		import_name_override.text = data.name
+		import_name_warning.visible = check_character_existance()
+		$ConfirmationDialog.popup_centered()
+		return
+
+func check_character_existance() -> bool:
+	var dir:= DirAccess.open("user://saves")
+	if dir==null || DirAccess.get_open_error()!=OK:
+		print("Can't open save dir!")
+		return false
+	
+	var file_name: String
+	dir.list_dir_begin()
+	file_name = dir.get_next()
+	while file_name!="":
+		file_name = file_name.left(file_name.rfind('.'))
+		if import_name_override.text==file_name:
+			return true
+		file_name = dir.get_next()
+	return false
+
+func _import_name_override_changed(new_name: String):
+	import_name_warning.visible = check_character_existance()
+
+func _import():
+	var dir:= DirAccess.open("user://saves/")
+	if dir==null || DirAccess.get_open_error()!=OK:
+		dir = create_save_dir()
+		if dir==null:
+			return
+	
+	var file: FileAccess
+	var pos:= import_data.find("\n")
+	var file_name: String = "user://saves/" + import_name_override.text + ".dat"
+	var data: Dictionary = JSON.parse_string(import_data.left(pos))
+	var name: String = data.name
+	# replace character name
+	import_data = import_data.replace(name, import_name_override.text)
+	file = FileAccess.open(file_name, FileAccess.WRITE)
+	if not file:
+		print("Can't open file " + file_name)
+		return
+	file.store_string(import_data)
+	file.close()
+	print("Imported file " + import_file)
+
 func create_instance(player_name: String):
 	main_instance = main.instantiate()
 	main_instance.player_name = player_name
@@ -131,6 +199,8 @@ func _ready():
 	
 	if OS.has_feature("web") || OS.has_feature("mobile"):
 		$Panel/VBoxContainer/Button3.hide()
+	
+	get_window().files_dropped.connect(Callable(self, "_files_dropped"))
 	
 	for i in range(35):
 		update_tile_map()
